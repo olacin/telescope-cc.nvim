5fe507c6eb537e0d05416f36e8cb2e6d96d8d7c5
Eric Neiford <19722607+eneiford@users.noreply.github.com>
2024-11-22 14:51:12 -0600

    feat: Added ability to mark breaking changes

--- lua/telescope/_extensions/conventional_commits/utils/format_commit_message.lua
+++ lua/telescope/_extensions/conventional_commits/utils/format_commit_message.lua
@@ -1,5 +1,6 @@
 return function(initial_message, inputs)
     local scope = inputs.scope
+    local breaking_change = inputs.breaking_change
     local msg = inputs.msg
 
     local commit_message = initial_message
@@ -7,6 +8,13 @@ return function(initial_message, inputs)
     if scope ~= nil and scope ~= "" then
         commit_message = commit_message .. string.format("(%s)", scope)
     end
+
+    if breaking_change == "y" then
+        commit_message = commit_message .. "!"
+    end
+
+    -- Escape the `!` character
+    commit_message = commit_message:gsub("!", "\\!")
 
     commit_message = string.format("%s: %s", commit_message, msg)
 
